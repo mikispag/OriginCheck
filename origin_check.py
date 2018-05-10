ce8a8afbb22ee3ba03800fe39c26004735718cda
Michele Spagnuolo <mikispag@gmail.com>
2018-05-10 09:23:08 +0200

    Increase NUM_WORKERS to 50.

--- origin_check.py
+++ origin_check.py
@@ -13,7 +13,7 @@ HEADERS = {
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.30 Safari/537.36'
 }
 MIN_RESPONSE_LENGTH = 100
-NUM_WORKERS = 10
+NUM_WORKERS = 50
 
 urls = []
 
